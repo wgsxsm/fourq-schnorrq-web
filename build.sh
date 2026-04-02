22247fca4512571a507abc7e0b952b04a115ebcf
lori <wgsxsm@gmail.com>
2026-04-02 19:37:31 +0800

    Fix build script to remove random.c reference

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ emcc -O3 -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap","ccall"]' \
      -s ALLOW_MEMORY_GROWTH=1 -s SINGLE_FILE=0 \
      -I. -I../sha512 -I../random \
      fourq_wasm.c schnorrq.c kex.c eccp2.c eccp2_core.c crypto_util.c hash_to_curve.c \
-     ../sha512/sha512.c ../random/random.c \
+     ../sha512/sha512.c \
      -o fourq.js \
      -D _GENERIC_ -D _AMD64_ -D __LINUX__ -D USE_ENDO
 echo "✅ 编译完成！生成 fourq.js 和 fourq.wasm"
