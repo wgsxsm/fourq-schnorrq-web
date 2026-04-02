b105c7a96f67fd16f893b86220d34baa91036b84
lori <wgsxsm@gmail.com>
2026-04-02 10:39:10 +0800

    Create build.sh for WebAssembly compilation
    Add build script for compiling WebAssembly module

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+emcc -O3 -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap","ccall"]' \
+     -s MODULARIZE=1 -s EXPORT_NAME='createFourQModule' \
+     -s ALLOW_MEMORY_GROWTH=1 -s SINGLE_FILE=0 \
+     -I. -I../sha512 -I../random \
+     fourq_wasm.c schnorrq.c kex.c eccp2.c eccp2_core.c crypto_util.c hash_to_curve.c \
+     ../sha512/sha512.c ../random/random.c \
+     -o fourq.js \
+     -D _GENERIC_ -D _AMD64_ -D __LINUX__ -D USE_ENDO
+echo "✅ 编译完成！生成 fourq.js 和 fourq.wasm"
